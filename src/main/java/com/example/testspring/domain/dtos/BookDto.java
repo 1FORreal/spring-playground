e0b84854337b09796b76dba678eb1ce0d4194996
1FORreal <biculescu.cezar@gmail.com>
2023-01-12 15:03:48 +0200

    Adding data validation to BookDto class and the necessary dependency

--- src/main/java/com/example/testspring/domain/dtos/BookDto.java
+++ src/main/java/com/example/testspring/domain/dtos/BookDto.java
@@ -1,5 +1,9 @@
 package com.example.testspring.domain.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,6 +14,12 @@ import lombok.*;
 @EqualsAndHashCode
 public class BookDto {
     private Long id;
+
+    @Size(min = 1, max = 30)
+    @NotNull @NotEmpty @NotBlank
     private String title;
+
+    @Size(min = 1, max = 200)
+    @NotNull @NotEmpty @NotBlank
     private String description;
 }
