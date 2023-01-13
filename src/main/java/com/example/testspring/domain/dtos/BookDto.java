2dd4fa527535bc4682769ef7792255cde535d90d
1FORreal <biculescu.cezar@gmail.com>
2023-01-13 11:13:07 +0200

    Adding proper error messages on controller input validation and other validation on entity

--- src/main/java/com/example/testspring/domain/dtos/BookDto.java
+++ src/main/java/com/example/testspring/domain/dtos/BookDto.java
@@ -15,11 +15,15 @@ import lombok.*;
 public class BookDto {
     private Long id;
 
-    @Size(min = 1, max = 30)
-    @NotNull @NotEmpty @NotBlank
+    @Size(min = 1, max = 30, message = "The title's length is not good!")
+    @NotNull(message = "Incorrect title!")
+    @NotEmpty(message = "Incorrect title!")
+    @NotBlank(message = "Incorrect title!")
     private String title;
 
-    @Size(min = 1, max = 200)
-    @NotNull @NotEmpty @NotBlank
+    @Size(min = 1, max = 200, message = "The description's length is not good!")
+    @NotNull(message = "Incorrect description!")
+    @NotEmpty(message = "Incorrect description!")
+    @NotBlank(message = "Incorrect description!")
     private String description;
 }
